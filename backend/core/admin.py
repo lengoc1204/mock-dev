97fb3c66f15ab08bbdf1fd75da5b7a644f39818a
lengoc12 <52289366+lengoc12@users.noreply.github.com>
2023-05-22 23:56:07 +0700

    first commit :'(

---
+++ backend/core/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django import forms
+from .models import User
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label=_("Password"),
+        help_text=_("Raw passwords are not stored, so there is no way to see "
+                    "this user's password, but you can change the password "
+                    "using <a href=\"password/\">this form</a>."))
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        f = self.fields.get('user_permissions', None)
+        if f is not None:
+            f.queryset = f.queryset.select_related('content_type')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+      form = UserChangeForm
+      fieldsets = (
+          (None, {'fields': ('email', 'password', )}),
+          (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+          (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                         'groups', 'user_permissions')}),
+          (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+            (_('user_info'), {'fields': ('birth_date', 'phone_number')}),
+      )
+      add_fieldsets = (
+          (None, {
+              'classes': ('wide', ),
+              'fields': ('email', 'password1', 'password2'),
+          }),
+      )
+      list_display = ['email', 'first_name', 'last_name', 'is_staff', "phone_number", 'birth_date']
+      search_fields = ('email', 'first_name', 'last_name', 'phone_number')
+      ordering = ('email', )
+
+
+admin.site.register(User, UserAdmin)
---
+++ backend/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+class User(AbstractUser):
+      username = models.CharField(max_length=50, unique=True)
+      email = models.EmailField(_('email address'), unique=True)
+      phone_number = models.CharField(max_length=11, blank=True, null=True)
+      birth_date = models.DateField(null=True, blank=True)
+      avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+      USERNAME_FIELD = 'email'
+      REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+      def __str__(self):
+          return "{}".format(self.email)
